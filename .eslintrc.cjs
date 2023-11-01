531ed08e491b6088cb09b6e58fb521a1db63c25b
Promchai Chooseang <promchai2sin@gmail.com>
2023-11-01 21:43:12 +0700

    feat: added tailwind and eslint plugin
    - Add Tailwind CSS
    - Add many ESLint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,10 @@ module.exports = {
 		'eslint:recommended',
 		'plugin:@typescript-eslint/recommended',
 		'plugin:svelte/recommended',
-		'prettier'
+		'prettier',
+		'plugin:tailwindcss/recommended',
+		'plugin:import/recommended',
+		'plugin:import/typescript'
 	],
 	parser: '@typescript-eslint/parser',
 	plugins: ['@typescript-eslint'],
@@ -26,5 +29,41 @@ module.exports = {
 				parser: '@typescript-eslint/parser'
 			}
 		}
-	]
+	],
+	rules: {
+		'sort-imports': [
+			'warn',
+			{
+				ignoreCase: false,
+				ignoreDeclarationSort: true,
+				ignoreMemberSort: false,
+				memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+				allowSeparatedGroups: false
+			}
+		],
+		'import/no-duplicates': 'off',
+		'import/no-unresolved': 'off',
+		'import/order': [
+			'warn',
+			{
+				groups: ['builtin', 'external', 'internal', ['sibling', 'parent'], 'index', 'unknown'],
+				'newlines-between': 'always',
+				alphabetize: {
+					order: 'asc',
+					caseInsensitive: true
+				}
+			}
+		]
+	},
+	settings: {
+		'import/parsers': {
+			'@typescript-eslint/parser': ['.ts', '.tsx'],
+			'svelte-eslint-parser': ['*.svelte']
+		},
+		'import/resolver': {
+			typescript: {
+				alwaysTryTypes: true
+			}
+		}
+	}
 };
